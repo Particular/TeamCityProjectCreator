31685efa388850914ad159e03542c8c1b5071ffa
SzymonPobiega <szymon.pobiega@gmail.com>
2015-05-06 14:05:04 +0200

    Handling . (dot) in project names in a decent way

--- src/TeamCityProjectCreator/CreateProject.ps1
+++ src/TeamCityProjectCreator/CreateProject.ps1
@@ -15,14 +15,21 @@ function Expand-Zip($file, $destination) {
 }
 
 function Replace-Variables($text) {
-    $result = $text.Replace("%PARENT_ID%",$ParentID).Replace("%PROJECT_NAME%",$ProjectName).Replace("%GUID()%",[Guid]::NewGuid())
+    $normalized_project_name = Normalize-ProjectName $ProjectName
+    $result = $text.Replace("%PARENT_ID%",$ParentID).Replace("%PROJECT_NAME%",$normalized_project_name).Replace("%GUID()%",[Guid]::NewGuid())
+    $result
+}
+
+function Normalize-ProjectName($project_name) {
+    $result = $project_name.Replace(".","_");
     $result
 }
 
 $source = "http://builds.particular.net/guestAuth/app/rest/builds/buildType:Tooling_BuildProcess_StandardNuGet,branch:master,status:SUCCESS/artifacts/content/template.zip"
 $project_folder = "C:\ProgramData\JetBrains\TeamCity\config\projects"
 
-$full_project_name = "${ParentID}_${ProjectName}"
+$normalized_project_name = Normalize-ProjectName $ProjectName
+$full_project_name = "${ParentID}_${normalized_project_name}"
 $full_project_path = Join-Path $project_folder $full_project_name
 if (Test-Path $full_project_path) {
     throw "Project with specified ID already exists"
